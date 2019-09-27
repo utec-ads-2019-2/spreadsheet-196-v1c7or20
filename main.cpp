fcc439190dbb137983d990df74f536fef4886b3b
v1c7or20 <victorpa793@gmail.com>
2019-09-26 20:32:05 -0500

    avance

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int spread_sheets = 0, rows = 0, col = 0;
+    string dato = "";
+    map<string, string> maps;
+    cin>>spread_sheets;
+    for (int i = 0; i < spread_sheets ;i++) {
+        cin>>col>>rows;
+        for (int j = 0; j < rows; j++) {
+            char name = 'A';
+            for (int k = 0; k < col; k++) {
+                cin>>dato;
+                string namae = "";
+                namae += name;
+                maps.insert(pair<string,string>(namae, dato));
+                name++;
+            }
+        }
+    }
+    cout<<"esta"<<endl;
     return 0;
 }
