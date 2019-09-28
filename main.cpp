15b3d5bdac10dbed233eb007938ff3c3ee9e5378
v1c7or20 <victorpa793@gmail.com>
2019-09-28 12:07:38 -0500

    second try

--- main.cpp
+++ main.cpp
@@ -1,99 +1,61 @@
 #include <iostream>
 #include <string>
-#include <map>
-#include <vector>
 #include <iomanip>
-#include <sstream>
+#include <memory.h>
 
 using namespace std;
 
-vector<std::string> split(string stringToBeSplitted, string delimeter)
-{
-    std::vector<std::string> splittedString;
-    int startIndex = 1;
-    int  endIndex = 0;
-    while( (endIndex = stringToBeSplitted.find(delimeter, startIndex)) < stringToBeSplitted.size() )
-    {
-        std::string val = stringToBeSplitted.substr(startIndex, endIndex - startIndex);
-        splittedString.push_back(val);
-        startIndex = endIndex + delimeter.size();
+
+string formula[10000][10000];
+int numb[10000][10000];
+
+int solve(int i,int j){
+    int r,c;
+    numb[i][j] = r = c = 0;
+    string str = formula[i][j];
+    for (int k = 1; k <str.size()+1 ; k++) {
+        if (isdigit(str.at(k))){
+            r = r*10+str.at(k)-'0';
+        } else if(str.at(k)>='A' and str.at(k) =='Z'){
+            c =c*26+str.at(k)-'A'+1;
+        }else if (str.at(k) == '+' or k == str.size()){
+            numb[i][j] +=solve(r,c);
+            r = c = 0;
+        }
+        return numb[i][j];
     }
-    if(startIndex < stringToBeSplitted.size())
-    {
-        std::string val = stringToBeSplitted.substr(startIndex);
-        splittedString.push_back(val);
-    }
-    return splittedString;
+    return 0;
 }
 
-void armar_nombre(string &namae, char letra){
-    char temp = letra;
-    char lul ;
-    do{
-        lul = ((temp-64 )%27)+64;
-        namae+=lul;
-        temp-=27;
-    }while (temp>64);
-}
-
-void solve(map<string,string> &mapa){
-    bool control = true;
-        while (control){
-            control = false;
-            for (auto &map : mapa) {
-                if ((*map.second.begin()) == '=') {
-                    vector<string> splited = split(map.second, "+");
-                    int valor = 0;
-                    for (auto &str : splited) {
-                        string palabra = mapa[str];
-                        if (palabra.size()>1 and isalpha(palabra.at(1))){
-                            control = true;
-                            break;
-                        }else{
-                            valor += atoi(mapa[str].c_str());
-                            control = false;
-                        }
-                    }
-                    if (!control){
-                        map.second = to_string(valor);
-                        control = true;
-                    }
+int main(){
+    int col,rows,spread;
+    cin>>spread;
+    for (int k= 0; k < spread; k++) {
+        cin>>col>>rows;
+        memset(numb,0, sizeof(numb));
+        for (int i = 0; i <rows ; i++) {
+            for (int j = 0; j < col; j++) {
+                string dato;
+                cin>>dato;
+                if (dato.at(0) == '='){
+                    formula[i][j] = dato;
+                    numb[i][j] = -10000;
+                } else{
+                    numb[i][j] = atoi(dato.c_str());
                 }
             }
         }
-}
-
-int main() {
-    int spread_sheets = 0, rows = 0, col = 0;
-    map<string, string> maps;
-    string dato = "";
-    cin>>spread_sheets;
-    for (int i = 0; i < spread_sheets ;i++) {
-        cin >> col >> rows;
-        for (int j = 0; j < rows; j++) {
-            char name = 65;
-            for (int k = 0; k < col; k++) {
-                cin >> dato;
-                string namae = "";
-                armar_nombre(namae, name);
-                namae += to_string(j + 1);
-                maps.insert(pair<string, string>(namae, dato));
-                name++;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (numb[i][j] == -10000)
+                    solve(i, j);
             }
         }
-        solve(maps);
-
-        for (int j = 0; j < rows; j++) {
-            char name = 65;
-            for (int k = 0; k < col; k++) {
-                string namae = "";
-                armar_nombre(namae, name);
-                namae += to_string(j + 1);
-                cout << setw(2) << maps[namae] << " ";
-                name++;
-            }cout << endl;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < col; ++j) {
+                cout<<setw(2)<<numb[i][j];
+            }cout<<endl;
         }
-        maps.clear();
     }
     return 0;
 }
